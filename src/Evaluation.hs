3809bacf8d3cab5f234e31eda95666b680c3bc33
peng fu <fermat1217@gmail.com>
2020-02-04 23:45:43 -0400

    use strict map to make memory grow slower and hex can run large_board.

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -17,8 +17,8 @@ import Control.Monad.State
 import Control.Monad.Identity
 import Control.Monad.Except
 
-import qualified Data.Map as Map
-import Data.Map (Map)
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Debug.Trace
