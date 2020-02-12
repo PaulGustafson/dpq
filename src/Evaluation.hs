262770f0d5846dd77daf76a22a73ad823ec1b587
frank <peng.frank.fu@gmail.com>
2020-02-11 21:19:07 -0400

    minor

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -21,8 +21,8 @@ import TypeError
 import Control.Monad.Identity
 import Control.Monad.Except
 import qualified Control.Monad.State as St
-import qualified Data.Map.Strict as Map
-import Data.Map.Strict (Map)
+import qualified Data.Map as Map
+import Data.Map (Map)
 import Data.Set (Set)
 import Data.List
 import qualified Data.Set as S
